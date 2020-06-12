4a8cef13ad59308d85d615705bec5be9707ee32f
helgakern <helgakern@gmail.com>
2020-06-11 21:11:34 -0700

    updated welcome and index

---
+++ voluntr_client/src/components/Welcome.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export class Welcome extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <main className="Welcome">
+                <h1>Welcome to Voluntr!
+                    <br />
+                    Volunteering 101 App.
+                    <br />
+                    Connecting helpers to those who need it.
+                </h1>
+            </main>
+        );
+    }
+}
--- voluntr_client/src/index.js
+++ voluntr_client/src/index.js
@@ -2,25 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-
-function OpportunitiesList(props){
-  return(
-    <div>
-      <h2>{props.title}</h2>
-      <p>{props.body}</p>
-    </div>
-  )
-}
+import App from "./components/App";
 
 ReactDOM.render(
-  <OpportunitiesList 
-    title="Opportunities for you"
-    body="Become a volunteer"
-  />,
-  document.getElementById('root')
+  <App />,
+  document.querySelector("#root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
