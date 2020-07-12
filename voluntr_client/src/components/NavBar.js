de20f96b74f1e27498ea3dfb0b0726fa00a760e1
helgakern <helgakern@gmail.com>
2020-07-11 20:32:19 -0700

    working on web design

--- voluntr_client/src/components/NavBar.js
+++ voluntr_client/src/components/NavBar.js
@@ -1,11 +1,6 @@
-import React from "react";
-import {
-  NavLink /* Link */
-} from "react-router-dom";
-import {
-  Menu,
-  Segment
-} from "semantic-ui-react";
+import React, { Component } from "react";
+import { NavLink /* Link */ } from "react-router-dom";
+import { FaAlignRight } from 'react-icons/fa';
 
 
 function NavBar(props) {
@@ -24,85 +19,35 @@ function NavBar(props) {
   };
   return (
 
-    <
-    div className = "ui inverted vertical center aligned segment" >
-    <
-    div className = "ui large inverted pointing secondary menu" >
-    <
-    div className = "ui container" >
-    <
-    NavLink to = "/"
-    className = "active item" >
-    <
-    img src = "https://i.pinimg.com/originals/ae/43/8b/ae438bc37a029cc195588a7fd8002808.jpg"
-    height = "50px" /
-    >
-    <
-    /NavLink>
+    <div className = "ui inverted vertical center aligned segment" >
+      <div className = "ui large inverted pointing secondary menu" >
+        <div className = "ui container" >
+          <NavLink to = "/" className = "active item" >
+          <img src = "https://i.pinimg.com/originals/ae/43/8b/ae438bc37a029cc195588a7fd8002808.jpg" height = "50px" />
+          </NavLink>
 
     {
-      /* <div className="right menu"> */ } <
-    NavLink to = "/"
-    className = "item" >
-    Home <
-    /NavLink> <
-    NavLink to = "/opportunities"
-    className = "item" >
-    Opportunities <
-    /NavLink> {
-      currentUser ? ( <
-        >
-        <
-        NavLink exact to = "/opportunities/new"
-        className = "item" >
-        Create an Opportunity <
-        /NavLink> <
-        NavLink to = "/opportunities"
-        onClick = {
-          onSignOut
-        }
-        className = "item" >
-        Sign Out <
-        /NavLink> <
-        span className = "item"
-        style = {
-          {
-            color: "teal"
-          }
-        } >
-        Welcome {
-          currentUser.full_name
-        } <
-        /span> <
-        />
-      ) : ( <
-        div className = "right item" >
-        <
-        React.Fragment >
-        <
-        NavLink exact to = "/sign_in"
-        className = "ui inverted button"
-        role = "button" >
-        Sign In <
-        /NavLink> <
-        NavLink exact to = "/sign_up"
-        className = "ui inverted button"
-        role = "button"
-        style = {
-          {
-            marginLeft: "0.5em"
-          }
-        } >
-        Sign Up <
-        /NavLink> <
-        /React.Fragment> <
+      /* <div className="right menu"> */ } 
+          <NavLink to = "/" className = "item" > Home </NavLink> 
+          <NavLink to = "/opportunities" className = "item" > Opportunities </NavLink> {
+            currentUser ? ( < >
+          <NavLink exact to = "/opportunities/new" className = "item" > Create an Opportunity </NavLink> 
+          <NavLink to = "/opportunities" onClick = {onSignOut} className = "item" > Sign Out </NavLink> 
+          <span className = "item" style = {{ color: "teal"}}> Welcome { currentUser.full_name } 
+          </span> 
+          < />
+      ) : ( 
+        <div className = "right item" >
+          <React.Fragment >
+            <NavLink exact to = "/sign_in" className = "ui inverted button" role = "button" > Sign In </NavLink> 
+            <NavLink exact to = "/sign_up" className = "ui inverted button" role = "button" style = {{ marginLeft: "0.5em" }} > Sign Up </NavLink> 
+          </React.Fragment> <
         /div>  
-      )
-    } <
-    /div> <
-    /div> <
-    /div>
-  );
+      )} 
+    </div> 
+  </div> 
+</div>
+);
 }
 
 export default NavBar;
